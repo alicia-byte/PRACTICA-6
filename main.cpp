e793da18bcdbcf861cfe8ab1349d6765471fac6d
alicia-byte <alicia.varon@estudiantat.upc.edu>
2025-04-24 15:28:21 +0200

    P6_E2

--- main.cpp
+++ main.cpp
@@ -1,42 +1,33 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <SD.h>
 
-#define SPI_SCK    13 
-#define SPI_MISO   12  
-#define SPI_MOSI   11  
-#define SPI_CS     10
-
-SPIClass mySPI(HSPI);  
-File myFile; 
-
-void setup() 
-{ 
-  Serial.begin(9600); 
-  Serial.print("Iniciando SD ..."); 
-  
-  mySPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI, SPI_CS);
-
-  if (!SD.begin(SPI_CS,mySPI)) {  // Usamos SPI_CS para CS
-    Serial.println("No se pudo inicializar la tarjeta SD");
-    return; 
-  }
-
-  Serial.println("Inicialización exitosa"); 
-    
-  myFile = SD.open("archivo.txt");
-  if (myFile) { 
-    Serial.println("archivo.txt:"); 
-    while (myFile.available()) { 
-      Serial.write(myFile.read()); 
-    } 
-    myFile.close(); // Cerramos el archivo 
-  } else { 
-    Serial.println("Error al abrir el archivo"); 
-  } 
+#include <SPI.h> 
+#include <MFRC522.h> 
+#define RST_PIN 9    //Pin 9 para el reset del RC522 
+#define SS_PIN 10   //Pin 10 para el SS (SDA) del RC522 
+MFRC522 mfrc522(SS_PIN, RST_PIN); //Creamos el objeto para el RC522 
+void setup() { 
+Serial.begin(115200); //Iniciamos la comunicación  serial 
+SPI.begin();        //Iniciamos el Bus SPI 
+mfrc522.PCD_Init(); // Iniciamos  el MFRC522 
+Serial.println("Lectura del UID"); 
 } 
-
-void loop() 
-{ 
-  // Aquí puedes agregar código que necesites ejecutar en el bucle.
-}
+void loop() { 
+// Revisamos si hay nuevas tarjetas  presentes 
+if ( mfrc522.PICC_IsNewCardPresent())  
+        {   
+//Seleccionamos una tarjeta 
+            if ( mfrc522.PICC_ReadCardSerial())  
+            { 
+                  // Enviamos serialemente su UID 
+                  Serial.print("Card UID:"); 
+                  for (byte i = 0; i < mfrc522.uid.size; i++) {
+                    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                      Serial.print(mfrc522.uid.uidByte[i], HEX);  
+                       
+              }  
+              Serial.println(); 
+              // Terminamos la lectura de la tarjeta  actual 
+              mfrc522.PICC_HaltA();          
+        } 
+      }
+    }
